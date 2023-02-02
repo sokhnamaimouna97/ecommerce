fe8e68f99657e745f6e20a2af124c482afb0ad6d
sokhna <sokhna-maimouna.thiam@atos.net>
2023-02-02 15:21:38 +0000

    first commit

---
+++ src/test/java/sn/atos/project/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.atos.project.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
